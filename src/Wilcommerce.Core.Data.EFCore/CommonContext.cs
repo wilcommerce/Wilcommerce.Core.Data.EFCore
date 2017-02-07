0c588ddb9fde15d5b2077cc32d667cfbb009f0b9
albi.mori <albi.mori@gmail.com>
2017-02-07 22:43:00 +0100

    add context, readmodel and repository implementation

---
+++ src/Wilcommerce.Core.Data.EFCore/CommonContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wilcommerce.Core.Common.Domain.Events;
+using Wilcommerce.Core.Common.Domain.Models;
+
+namespace Wilcommerce.Core.Data.EFCore
+{
+    public class CommonContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+
+        public virtual DbSet<GeneralSettings> Settings { get; set; }
+
+        public virtual DbSet<EventWrapper> Events { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/Wilcommerce.Core.Data.EFCore/Repository/Repository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wilcommerce.Core.Common.Domain.Repository;
+
+namespace Wilcommerce.Core.Data.EFCore.Repository
+{
+    public class Repository : IRepository
+    {
+        protected CommonContext _context;
+
+        public Repository()
+        {
+            _context = new CommonContext();
+        }
+
+        public void Dispose()
+        {
+            if (_context != null)
+            {
+                _context.Dispose();
+            }
+
+            GC.SuppressFinalize(this);
+        }
+
+        public void SaveChanges()
+        {
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch 
+            {
+                throw;
+            }
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void Add<TModel>(TModel model) where TModel : class, Infrastructure.IAggregateRoot
+        {
+            try
+            {
+                _context.Set<TModel>().Add(model);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public TModel GetByKey<TModel>(Guid key) where TModel : class, Infrastructure.IAggregateRoot
+        {
+            try
+            {
+                return _context.Find<TModel>(key);
+            }
+            catch 
+            {
+                throw;
+            }
+        }
+
+        public async Task<TModel> GetByKeyAsync<TModel>(Guid key) where TModel : class, Infrastructure.IAggregateRoot
+        {
+            try
+            {
+                var model = await _context.FindAsync<TModel>(key);
+                return model;
+            }
+            catch 
+            {
+                throw;
+            }
+        }
+    }
+}
