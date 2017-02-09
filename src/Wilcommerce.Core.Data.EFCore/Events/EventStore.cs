95acefb030e6db93178b582d02217155abbbd2fe
albi.mori <albi.mori@gmail.com>
2017-02-09 21:35:01 +0100

    event store implementation

---
+++ src/Wilcommerce.Core.Data.EFCore/Events/EventStore.cs
@@ -0,0 +1,26 @@
+﻿using Wilcommerce.Core.Common.Domain.Events;
+using Wilcommerce.Core.Infrastructure;
+
+namespace Wilcommerce.Core.Data.EFCore.Events
+{
+    public class EventStore : IEventStore
+    {
+        public void Save<TEvent>(TEvent @event) where TEvent : DomainEvent
+        {
+            using (var context = new CommonContext())
+            {
+                try
+                {
+                    var ev = EventWrapper.Wrap(@event);
+                    context.Events.Add(ev);
+
+                    context.SaveChanges();
+                }
+                catch 
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
